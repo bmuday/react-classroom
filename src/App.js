afbc2e147b7f8ac77ae3f7b548182548bb2a186f
bmuday <bmuday@live.fr>
2023-05-22 09:33:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
+  // Constantes
+  const nbStudents = 16; // number
+  const name = "Baptiste"; // string
+  const city = "Villejuif";
+  const flowers = ["tulipe", "rose"]; // array
+  const classroom = { // object
+    name: "React Classroom",
+    nbStudents,
+  };
+
+  // CSS
+  let style = {
+    paragraph1: {backgroundColor: "yellow"}, //camelCase
+    paragraph2: {backgroundColor: "red"},
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Header */}
+      <Header/>
+      {/* Reste de la page */}
+      <ul>
+        {flowers.map((flower, index) => (
+          <li key={index}>{flower}</li>
+        ))}
+      </ul>
+      <p style={style.paragraph1}>
+        {name} is teacher of a class of {nbStudents} students.
+      </p>
+      <p style={style.paragraph2}>He lives in {city}.</p>
+      <table>
+        <th>Classroom</th>
+        <tr>
+          <td>{classroom.name}</td>
+        </tr>
+        <tr>
+          <td>{classroom.nbStudents}</td>
+        </tr>
+      </table>
     </div>
   );
 }
