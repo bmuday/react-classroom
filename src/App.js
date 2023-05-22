5f55e37ef5120080f78cc9c2886cf76b0acb5cd1
bmuday <bmuday@live.fr>
2023-05-22 09:48:21 +0200

    create a section component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 function App() {
-  // Constantes
-  const nbStudents = 16; // number
-  const name = "Baptiste"; // string
-  const city = "Villejuif";
-  const flowers = ["tulipe", "rose"]; // array
-  const classroom = { // object
-    name: "React Classroom",
-    nbStudents,
-  };
-
-  // CSS
-  let style = {
-    paragraph1: {backgroundColor: "yellow"}, //camelCase
-    paragraph2: {backgroundColor: "red"},
-  }
   return (
     <div>
       {/* Header */}
-      <Header/>
+      <Header />
       {/* Reste de la page */}
-      <ul>
-        {flowers.map((flower, index) => (
-          <li key={index}>{flower}</li>
-        ))}
-      </ul>
-      <p style={style.paragraph1}>
-        {name} is teacher of a class of {nbStudents} students.
-      </p>
-      <p style={style.paragraph2}>He lives in {city}.</p>
-      <table>
-        <th>Classroom</th>
-        <tr>
-          <td>{classroom.name}</td>
-        </tr>
-        <tr>
-          <td>{classroom.nbStudents}</td>
-        </tr>
-      </table>
+      <Section />
     </div>
   );
 }
